83179ba02be674f326c73a3d4db5e757a2e88da3
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-06-07 16:49:06 -0700

    Refactor the code

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ const App = () => {
   const fetchNews = () => {
     setLoading(true)
     fetch(url)
-    .then(result => result.json())
-    .then(data => (setNews(data.hits), setLoading(false)))
-    .catch(error => console.log(error))
-    
+      .then(result => result.json())
+      .then(data => (setNews(data.hits), setLoading(false)))
+      .catch(error => console.log(error))
+
   };
 
   useEffect(() => {
-    setTimeout(fetchNews(), 2000) 
+    setTimeout(fetchNews(), 2000)
   }, [url]);
 
   const handleChange = (e) => {
@@ -30,26 +30,38 @@ const App = () => {
     setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
   }
 
-  return (
-    <div className="container">
-      <h2><i className="fas fa-newspaper" />{" "}Hacker News API</h2>
+  const showForm = () => {
+    return (
       <form className="form-inline" onSubmit={handleSubmit}>
         <div className="form-group">
           <label for="keyword" className="mr-2 mb-2">Enter a keyword</label>
           <input type="text" className="form-control mr-2 mb-2" value={searchQuery} onChange={handleChange} />
         </div>
         <button className="btn btn-primary btn-sm mb-2">Search</button>
-        {loading ?<div class="spinner-border m-2" role="status">
+        {loading ? <div class="spinner-border m-2" role="status">
           <span class="sr-only">Loading...</span>
         </div> : ""
         }
       </form>
+    );
+  }
+
+  const showNews = () => {
+    return (
       <div>
         {
           news.map((n, i) =>
             (<p key={i}><i className="fas fa-caret-right" />{" "}{n.title}{" | "}<a href={n.url}>Post</a></p>))
         }
       </div>
+    );
+  }
+
+  return (
+    <div className="container">
+      <h2><i className="fas fa-newspaper" />{" "}Hacker News API</h2>
+      {showForm()}
+      {showNews()}
     </div>
   );
 }
